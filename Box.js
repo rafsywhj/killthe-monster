c0fab99421b782603577290fbc6e3b585dbdc3e4
rafsywhj <70297474+rafsywhj@users.noreply.github.com>
2021-08-14 14:35:08 +0400

    Update Box.js

--- Box.js
+++ Box.js
@@ -15,10 +15,10 @@ class Box{
         var angle=this.body.angle
         push();
         translate(pos.x,pos.y)
-        rotate(angle)
+        rotate(angle);
         rectMode(CENTER)
         strokeWeight(4)
-        stroke("black")
+        stroke("darkblue")
         fill("red")
         rect(0,0,this.width,this.height)
         pop()
